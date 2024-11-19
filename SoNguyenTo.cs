10e4fd37d72f6de8200372e2631c594243ff865c
Hungdzpro2506 <134621730+Hungdzpro2506@users.noreply.github.com>
2024-11-19 15:43:12 +0700

    Add files via upload

---
+++ SoNguyenTo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dem = 0;
+            Console.WriteLine("Danh Sach so NT tu 0-100 la: ");
+            for (int i = 2;i<=100;i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        dem++;
+                    }
+                    
+                }
+                if (dem == 2) { Console.WriteLine(i); }
+                dem = 0;
+            }
+        }
+    }
+}
