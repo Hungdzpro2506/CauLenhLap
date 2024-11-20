f7fcc7368430d85f46e5b2cce58b170eab457df3
Hungdzpro2506 <134621730+Hungdzpro2506@users.noreply.github.com>
2024-11-20 10:05:43 +0700

    Add files via upload

---
+++ 20soNT.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nhap so luong so nguyen to muon in ra: ");
+            int Number = Int32.Parse(Console.ReadLine());
+            int Count = 0;
+            int N = 2;
+            int Dem = 0;
+            while(Count < Number)
+            {
+                bool SoNT = true;
+                if (N < 2)
+                {
+                    SoNT = false;
+                }
+                else
+                {
+                    int CanBac2N = (int)Math.Sqrt(N);
+                    for(int i = 2;i <= CanBac2N;i++)
+                    {
+                        if(N%i == 0)
+                        {
+                            SoNT = false;
+                            break;
+                        }
+                    }
+                }
+                if( SoNT )
+                {
+                    Count++;
+                    Console.WriteLine(N);
+                }
+                N++;
+
+            }
+
+        }
+    }
+}
